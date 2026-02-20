ca67e855642f69fa4b15b0234f9997eeef377bc7
panos <panos@panosbampalis.com>
2026-02-20 19:32:57 +0200

    fix5

--- app/tours/nafplio/page.tsx
+++ app/tours/nafplio/page.tsx
@@ -162,7 +162,7 @@ const culinaryTours = [
     duration: "2.5 - 3 hours",
     price: "€75",
     type: "For All (not recommended for children under 10 y.o.)",
-    image: "/images/beekeeping-tour-hero-1",
+    image: "/images/beekeeping-tour-hero-1.jpg",
     icon: Compass,
     highlights: ["Beekeeping museum", "Bee gardens", "Honey tasting"],
   },
